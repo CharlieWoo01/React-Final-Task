29b9792d24aae65505fe478ca7adce66ca527c96
Charlie Roberts <charliewoo1412@gmail.com>
2023-05-09 16:53:04 +0100

    Add shopping basket totals

--- src/Components/Products/ProductList.js
+++ src/Components/Products/ProductList.js
@@ -25,8 +25,11 @@ function ProductList(props) {
     });
   }
 
-  // Function to add to the basket
-  function addToBasket(product, index) {
+  /**
+   * Function to add to the basket
+   * @todo: Look to refactor this as it is a massive function now and fairly complex
+   */
+  function addToBasket(product, index, price) {
     const basketItems = JSON.parse(localStorage.getItem("basketItems")) || {};
     const basketItemsArray = Object.values(basketItems);
     const existingItemIndex = basketItemsArray.findIndex(
@@ -36,7 +39,8 @@ function ProductList(props) {
     // If in basket already then add quantity + 1
     if (existingItemIndex !== -1) {
       const updatedItems = { ...basketItems };
-      updatedItems[product.id].quantity += 1;
+      updatedItems[product.id].quantity += 1; // Add quantity if exists
+      updatedItems[product.id].totalPrice += price; // Add up the total price of each product
       localStorage.setItem("basketItems", JSON.stringify(updatedItems));
     }
     // If not in basket already then add it and initiate the new object and basket change
@@ -45,7 +49,7 @@ function ProductList(props) {
         "basketItems",
         JSON.stringify({
           ...basketItems,
-          [product.id]: { ...product, quantity: 1 },
+          [product.id]: { ...product, quantity: 1, totalPrice: price},
         })
       );
       setBasketCount(basketCount + 1);
@@ -55,7 +59,12 @@ function ProductList(props) {
     handleStockChange(index); // Execute the stock change function
   }
 
+
   //  localStorage.clear();
+
+  /**
+   * @todo: Possibly look at alternatives of neating this up as a lot of inline conditionals
+   */
   return (
     <>
       <MainNavigation />
@@ -67,18 +76,28 @@ function ProductList(props) {
             </div>
             <div className="card-body">
               <div className="card-information">
-                <p>Price: £{product.UnitPrice}</p>
-                <p>Stock: {stockCount[index]}</p>
+                {window.location.pathname === "/products" ? (
+                  <>
+                    <p>Price: £{product.UnitPrice}</p>
+                    <p>Stock: {stockCount[index]}</p>
+                  </>
+                ) : (
+                  <>
+                    <p>Total Price: £{ product.totalPrice }</p>
+                    <p>Quantity: { product.quantity }</p>
+                  </>
+                )}
               </div>
               <div className="button-container">
                 {window.location.pathname === "/products" ? (
                   <>
                     {stockCount[index] > 0 ? (
-                        <button className="add-basket-controls"
-                          onClick={() => addToBasket(product, index)}
-                        >
-                          Add to basket
-                        </button>
+                      <button
+                        className="add-basket-controls"
+                        onClick={() => addToBasket(product, index, product.UnitPrice)}
+                      >
+                        Add to basket
+                      </button>
                     ) : (
                       <p style={{ color: "red" }}>Out of stock</p>
                     )}
